37444d57aa3e86d4da25d823c25a6261b285e500
Kevin O'Connor <Kevin@MacBook-Pro.local>
2022-05-25 12:22:48 -0500

    Added RNG

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+extern crate rand;
+use rand::Rng;
+
+fn main(){
+    println!("Guess the number!");
+    let secret = rand::thread_rng().gen_range(1,101);
+    println!("Please input your guess.");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).
+    expect("Failed to read line");
+    println!("You guessed: {}", guess);
+    println!("The secret number is {}", secret)
 }
